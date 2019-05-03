259ef30ea5bb146aab56c746e6e97d2088b76f85
Jimmy Im <jim011@ucr.edu>
2019-05-02 19:36:30 -0700

    lab09 finished

--- jim011_lab09/jim011_lab09_part03/jim011_lab09_part03/main.c
+++ jim011_lab09/jim011_lab09_part03/jim011_lab09_part03/main.c
@@ -1,11 +1,92 @@
-#define NOTEC4 261.63
-#define NOTED4 293.66
-#define NOTEE4 329.63
-#define NOTEF4 349.23
-#define NOTEG4 392.00
-#define NOTEA4 440.00
-#define NOTEB4 493.88
-#define NOTEC5 523.25
+#define NOTE_B0  31.0
+#define NOTE_C1  33.0
+#define NOTE_CS1 35.0
+#define NOTE_D1  37.0
+#define NOTE_DS1 39.0
+#define NOTE_E1  41.0
+#define NOTE_F1  44.0
+#define NOTE_FS1 46.0
+#define NOTE_G1  49.0
+#define NOTE_GS1 52.0
+#define NOTE_A1  55.0
+#define NOTE_AS1 58.0
+#define NOTE_B1  62.0
+#define NOTE_C2  65.0
+#define NOTE_CS2 69.0
+#define NOTE_D2  73.0
+#define NOTE_DS2 78.0
+#define NOTE_E2  82.0
+#define NOTE_F2  87.0
+#define NOTE_FS2 93.0
+#define NOTE_G2  98.0
+#define NOTE_GS2 104.0
+#define NOTE_A2  110.0
+#define NOTE_AS2 117.0
+#define NOTE_B2  123.0
+#define NOTE_C3  131.0
+#define NOTE_CS3 139.0
+#define NOTE_D3  147.0
+#define NOTE_DS3 156.0
+#define NOTE_E3  165.0
+#define NOTE_F3  175.0
+#define NOTE_FS3 185.0
+#define NOTE_G3  196.0
+#define NOTE_GS3 208.0
+#define NOTE_A3  220.0
+#define NOTE_AS3 233.0
+#define NOTE_B3  247.0
+#define NOTE_C4  262.0
+#define NOTE_CS4 277.0
+#define NOTE_D4  294.0
+#define NOTE_DS4 311.0
+#define NOTE_E4  330.0
+#define NOTE_F4  349.0
+#define NOTE_FS4 370.0
+#define NOTE_G4  392.0
+#define NOTE_GS4 415.0
+#define NOTE_A4  440.0
+#define NOTE_AS4 466.0
+#define NOTE_B4  494.0
+#define NOTE_C5  523.0
+#define NOTE_CS5 554.0
+#define NOTE_D5  587.0
+#define NOTE_DS5 622.0
+#define NOTE_E5  659.0
+#define NOTE_F5  698.0
+#define NOTE_FS5 740.0
+#define NOTE_G5  784.0
+#define NOTE_GS5 831.0
+#define NOTE_A5  880.0
+#define NOTE_AS5 932.0
+#define NOTE_B5  988.0
+#define NOTE_C6  1047.0
+#define NOTE_CS6 1109.0
+#define NOTE_D6  1175.0
+#define NOTE_DS6 1245.0
+#define NOTE_E6  1319.0
+#define NOTE_F6  1397.0
+#define NOTE_FS6 1480.0
+#define NOTE_G6  1568.0
+#define NOTE_GS6 1661.0
+#define NOTE_A6  1760.0
+#define NOTE_AS6 1865.0
+#define NOTE_B6  1976.0
+#define NOTE_C7  2093.0
+#define NOTE_CS7 2217.0
+#define NOTE_D7  2349.0
+#define NOTE_DS7 2489.0
+#define NOTE_E7  2637.0
+#define NOTE_F7  2794.0
+#define NOTE_FS7 2960.0
+#define NOTE_G7  3136.0
+#define NOTE_GS7 3322.0
+#define NOTE_A7  3520.0
+#define NOTE_AS7 3729.0
+#define NOTE_B7  3951.0
+#define NOTE_C8  4186.0
+#define NOTE_CS8 4435.0
+#define NOTE_D8  4699.0
+#define NOTE_DS8 4978.0
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -106,21 +187,69 @@ void PWM_off() {
 	TCCR0A = 0x00;
 	TCCR0B = 0x00;
 }
+const unsigned char MAXINDEX = 78;
+//Mario main theme melody
+double melody[78] = {
+	NOTE_E7, NOTE_E7, 0, NOTE_E7,
+	0, NOTE_C7, NOTE_E7, 0,
+	NOTE_G7, 0, 0,  0,
+	NOTE_G6, 0, 0, 0,
+
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
+
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0,
+
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
+
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[78] = {
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+};
 
 
-
-enum States {start, init, wait, toggle, toggleRelease, upScale, upScaleRelease, downScale, downScaleRelease} state;
+enum States {start, init, wait, play} state;
 unsigned char tempA = 0x00;
 unsigned char button1 = 0x00;
-unsigned char button2 = 0x00;
-unsigned char button3 = 0x00;
-//unsigned short count = 0;
-const unsigned short PERIOD = 100;
-
-const char NOTEARRAYMAX = 7;
-double NOTEARRAY[8] = {NOTEC4,NOTED4,NOTEE4,NOTEF4,NOTEG4,NOTEA4,NOTEB4,NOTEC5};
+unsigned short count = 0;
+const unsigned short PERIOD = 200;
 unsigned char noteIndex;
-unsigned char isOn;
 double freqOut = 0.00;
 void Tick();
 
@@ -146,8 +275,6 @@ void Tick() {
 	tempA = (~PINA) & 0x07;
 	
 	button1 = tempA & 0x01;
-	button2 = (tempA & 0x02) >> 1;
-	button3 = (tempA & 0x04) >> 2;
 	//Transition
 	switch(state) {
 		case start:
@@ -159,45 +286,15 @@ void Tick() {
 		break;
 		
 		case wait:
-		if(button2 && !button1 && !button3) {
-			state = toggle;
-		}
-		else if(!button2 && button1 && !button3) {
-			state = upScale;
-		}
-		else if(!button2 && !button1 && button3) {
-			state = downScale;
+		if(button1){
+			state = play;
 		}
 		break;
 		
-		case toggle:
-		if(!button2 && !button1 && !button3) {
-			state = toggleRelease;
+		case play:
+		if(!button1 && noteIndex >= MAXINDEX - 1) {
+			state = wait;
 		}
-		break;
-		
-		case toggleRelease:
-		state = wait;
-		break;
-		
-		case upScale:
-		if(!button2 && !button1 && !button3) {
-			state = upScaleRelease;
-		}
-		break;
-		
-		case upScaleRelease:
-		state = wait;
-		break;
-		
-		case downScale:
-		if(!button2 && !button1 && button3) {
-			state = downScaleRelease;
-		}
-		break;
-		
-		case downScaleRelease:
-		state = wait;
 		break;
 		
 		default:
@@ -206,37 +303,25 @@ void Tick() {
 	//Action
 	switch(state) {
 		case init:
-
 		noteIndex = 0;
-		freqOut = NOTEARRAY[0];
-		isOn = 0;
+		count = 0;
 		break;
 		
 		case wait:
+		noteIndex = 0;
+		freqOut = 0;
+		count = 0;
 		break;
 		
-		case toggleRelease:
-		if(isOn) {
-			PWM_off();
+		case play:
+		if(tempo[noteIndex] >= count) {
+			if(noteIndex < MAXINDEX - 1) {
+				noteIndex++;
+				count = 0;
+			}
 		}
-		else {
-			PWM_on();
-		}
-		isOn = !isOn;
-		break;
-		
-		case upScaleRelease:
-		if(noteIndex < NOTEARRAYMAX) {
-			noteIndex++;
-		}
-		freqOut = NOTEARRAY[noteIndex];
-		break;
-		
-		case downScaleRelease:
-		if(noteIndex > 0) {
-			noteIndex--;
-		}
-		freqOut = NOTEARRAY[noteIndex];
+		freqOut = melody[noteIndex];
+		count++;
 		break;
 		
 		default:
@@ -244,3 +329,6 @@ void Tick() {
 	}
 	set_PWM(freqOut);
 }
+
+
+
